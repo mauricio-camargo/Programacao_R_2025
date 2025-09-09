3cf6f68b94dbe0223083cabdd6e58cc2de430054
mauricio-camargo <camargofurg@gmail.com>
2025-09-08 23:13:52 -0300

    nnn

--- Aula2.R
+++ Aula2.R
@@ -1,88 +1,36 @@
-#Gráficos simples
+Aula 05/09/2025 - Programação em R
 
-#Gerando dados aleatórios
-set.seed(21)
-v=rnorm(50,12,4)
+######## Exemplo 1: soma de dois números. ####
 
-#Construíndo um Histograma
-hist(v)#Função de alto nível
-text(5,8,'Texto')#Função baixo nível
-text(5,9,'Mais um')
-points(8,10)
-points(9,10)
+soma2num = function(x1,x2) {
+  x_tot = x1 + x2
+  return(x_tot)
+}
+# Testando a função
+soma2num(12,15)
 
-hist(v,ylab='Frequência',
-     xlab='Classes',
-     main='Meu histograma',
-     col='blue')
+####### Exemplo 2: Concatenar duas strings. ####
 
-hist(v,ylab='Frequência',
-     xlab='Classes',
-     main='Meu histograma',
-     col=c('blue','red','yellow','green'))
-text(3,2,'azul')
-text(5,4,'laranja')
-text(7,6,'amarelo')
-text(9,6,'verde')
-text(11,7,'azul de novo')
-#Tentar plotar com 13
-text(13,13,'asd')
-#Não aparece
-#Refazer o gráfico com ylim=c(0,15),
+alo_mundo = function(s1,s2) {
+  s = (paste(s1, s2))
+  return(s)
+}
+# Testando a função
+alo_mundo('Hello,','world!')
 
-#### Dividindo a janela ####
-set.seed(21)
-v=rnorm(50,12,4)
-par(mfrow=c(2,1))
-hist(v)
-boxplot(v,horizontal = T)
+####### Exercício 1: Multiplicar dois números. ####
+f_mult = function(x1,x2) {
+  xt = x1 * x2
+  return(xt)
+}
+# Testando a função
+f_mult(12,2)
 
-#### Dados transformados ####
-par(mfrow=c(2,1))
-hist(log(v))
-boxplot(log(v),horizontal=T,outline = F)
-par(mfrow=c(1,1))
-qqnorm(v)
-qqline(v)
-
-#### Abrindo arquivo Excel ####
-library(readxl)
-dados=read_excel('planilha1.xlsx') #O arquivo do script (Aula2.R) e do Excel têm que estar na mesma pasta.
-#Pode-se usar a funcionalidade "Import dataset" do Rstudio.
-dados
-dados$dep
-dados$fator1
-hist(dados$dep)
-boxplot(dados$dep)
-
-
-#### Gráficos básicos ####
-#Criando um gráfico de barras sem dispersão
-table(dados$fator1)
-barplot(table(dados$fator1))
-
-#Gráficos qqplot
-qqnorm(dados$dep)
-qqline(dados$dep)
-
-#Graficando com erro padrão
-library(sciplot)
-bargraph.CI(dados$fator1,dados$dep,lc=F,ylim=c(0,25))
-abline(h=0)
-
-dados
-dados$fator2
-
-bargraph.CI(fator1,dep, factor(fator2),data=dados)
-
-text(1.5,20,'A1')
-text(2.5,26,'A2') #Fica fora
-
-#Usando ylim()
-bargraph.CI(fator1,dep, factor(fator2), ylim=c(0,28),data=dados)
-text(1.5,20,'A1')
-text(2.5,27,'A2') #Agora está dentro
-
-text(4.5,15,'B1')
-text(5.5,14,'B2')
+####### Exercício 2: Calcular a circunferência. ####
+f_circ = function(r) {
+  cincunferencia = 2*pi*r
+  return(cincunferencia)
+}
+# Testando a função
+f_circ(1)
 
