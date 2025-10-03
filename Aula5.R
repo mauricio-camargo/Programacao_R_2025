5c8c01b54c041671eeac901cc81cd9ab18233485
mauricio-camargo <camargofurg@gmail.com>
2025-10-03 12:05:48 -0300

    asdasd

---
+++ Aula5.R
@@ -0,0 +1,58 @@
+#### Exercício 2
+m=matrix(1:16,nrow=4)
+m
+
+#### Exercício 3
+m[2,3] = 0
+m
+
+#### Exercício 4
+soma=0
+for (i in 1:(nrow(m))) 
+  for (j in 1:(ncol(m))) 
+    soma = soma + m[i, j]
+
+soma    
+
+#### Exercício 5
+v_soma = c()
+for (i in 1:(nrow(m))) {
+  ac = 0
+  for (j in 1:(ncol(m))) {
+    ac = ac + m[i, j]
+  }
+  v_soma[i] = ac
+}
+
+v_soma
+
+#### Exercício 6
+cont = 0
+for (i in 1:(nrow(m))) 
+  for (j in 1:(ncol(m))) 
+    if (m[i,j] > 12)
+     cont = cont + 1
+
+cont
+     
+#### Exercício 7
+f_diag = function(mat) { 
+vet = c()
+for (i in 1:(nrow(m))) 
+  for (j in 1:(ncol(m))) 
+   if (i==j)
+     vet=c(vet,m[i,j])
+return(vet)
+}
+f_diag(m)
+
+
+#### Exercício 8
+f_tab = function(a,b) {
+for (n1 in a:b) {
+  print(paste('Tabuada do ', n1))
+  for (n2 in a:b)
+    print(paste(n1, 'x', n2, ' = ', n1 * n2))
+}
+}
+f_tab(1,3)
