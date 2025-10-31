9aa516715dc6ff997fe062c037bbcb4a52bc57dd
mauricio-camargo <camargofurg@gmail.com>
2025-10-31 08:29:47 -0300

    aSASD

--- Aula8_Exerc_Resolvidos.R
+++ Aula8_Exerc_Resolvidos.R
@@ -1,6 +1,20 @@
 dados = readxl::read_excel('plan1.xlsx')
 dados
 
+# Média da temperatura por local
 aggregate(temp ~ local, data = dados, FUN = mean)
 
+# Média da salinidade por local E estação
+aggregate(sal ~ local + estacao, data = dados, FUN = mean)
 
+# Soma da abundância por estação
+aggregate(sp1 ~ estacao, data = dados, FUN = sum)
+
+# Quantas amostras tenho por local?
+aggregate(temp ~ local, data = dados, FUN = length)
+
+ aggregate(dados[, c("temp", "sal", "pH")],
+           by = list(Local = dados$local,
+                     Estacao = dados$estacao),
+           FUN = mean)
+ 
