6890a858ec859a4908999593ed39e1a75382f6b9
mauricio-camargo <camargofurg@gmail.com>
2025-10-31 08:36:58 -0300

    asd

--- Aula8_Exerc_Resolvidos.R
+++ Aula8_Exerc_Resolvidos.R
@@ -13,8 +13,12 @@ aggregate(sp1 ~ estacao, data = dados, FUN = sum)
 # Quantas amostras tenho por local?
 aggregate(temp ~ local, data = dados, FUN = length)
 
- aggregate(dados[, c("temp", "sal", "pH")],
+# Para selecionar várias colunas para mostrar: 
+aggregate(dados[, c("temp", "sal", "pH")],
            by = list(Local = dados$local,
                      Estacao = dados$estacao),
+           data = dados,
            FUN = mean)
- 
+
+dad = readxl::read_excel('limno.xlsx')
+dad
